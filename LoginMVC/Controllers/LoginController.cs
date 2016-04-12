1b32699c847915982313fac8a8e46c0ec1eb94d5
caofangsheng93 <fangsheng93@163.com>
2016-04-12 17:55:34 +0800

    Form验证登陆

--- LoginMVC/Controllers/LoginController.cs
+++ LoginMVC/Controllers/LoginController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace LoginMVC.Controllers
 {
@@ -30,7 +31,7 @@ namespace LoginMVC.Controllers
 
             LoginModel loginModel = new LoginModel()
             {
-                UserId=model.UserId,  //userID设置为了Key,既主键，EF会自动增长，这里添加的话，也不会报错。如果有UserID为1的记录我在添加也不会报错。加进去是递增的，是EF自动增长决定的，和模型绑定无关。
+                UserId = model.UserId,  //userID设置为了Key,既主键，EF会自动增长，这里添加的话，也不会报错。如果有UserID为1的记录我在添加也不会报错。加进去是递增的，是EF自动增长决定的，和模型绑定无关。
                 UserName = model.UserName,
                 Password = model.Password,
                 IsActive = model.IsActive
@@ -47,28 +48,90 @@ namespace LoginMVC.Controllers
             return View();
         }
 
+        #region V1.0版本
+        //V1.0版本
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Login([Bind(Include = "UserName,Password")] LoginModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+
+        //        using (var db = new LoginDbContext())
+        //        {
+        //            var loginModel = db.LoginModel.Where(s => s.UserName == model.UserName && s.Password == model.Password).FirstOrDefault();  
+
+        //            if (loginModel != null)
+        //            {
+        //                //设置Session
+        //                Session["UserID"] = loginModel.UserId.ToString();
+        //                Session["UserName"] = loginModel.UserName.ToString();
+        //                //登陆成功跳转到DashBoard页面
+        //                return RedirectToAction("UserDashBoard");
+        //            }
+        //        } 
+
+        //    }
+
+        //    return View(model);
+        //} 
+        #endregion
+
+
+        /// <summary>
+        /// v2.0登陆版本
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Login([Bind(Include = "UserName,Password")] LoginModel model)
+        public ActionResult Login([Bind(Include = "UserName,Password")] LoginModel model, string returnUrl)
         {
             if (ModelState.IsValid)
             {
-                using (var db = new LoginDbContext())
+
+                LoginModel loginUserModel = new LoginModel();
+                string pwd = GetUserPassword(model.UserName);
+                if (string.IsNullOrEmpty(pwd))
                 {
-                    var loginModel = db.LoginModel.Where(s => s.UserName == model.UserName && s.Password == model.Password).FirstOrDefault();
-                    if (loginModel != null)
+                    ModelState.AddModelError("", "用户名或密码错误，登陆失败，请联系管理员");
+                }
+                else
+                {
+                    //判断密码是否等于查出来的密码
+                    if (model.Password.Equals(pwd))
                     {
                         //设置Session
-                        Session["UserID"] = loginModel.UserId.ToString();
-                        Session["UserName"] = loginModel.UserName.ToString();
-                        //登陆成功跳转到DashBoard页面
-                        return RedirectToAction("UserDashBoard");
+                        Session["UserID"] = model.UserId.ToString();
+                        Session["UserName"] = model.UserName.ToString();
+                        FormsAuthentication.SetAuthCookie(model.UserName, false);
+                        return RedirectToAction("UserDashBoard", "Login");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "密码不正确");
                     }
                 }
+
             }
 
             return View(model);
-        } 
+        }
+
+
+        /// <summary>
+        /// 登出功能
+        /// </summary>
+        /// <returns></returns>
+        //[Authorize]
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Login");
+        }
+
+
         #endregion
 
         public ActionResult UserDashBoard()
@@ -82,5 +145,43 @@ namespace LoginMVC.Controllers
                 return RedirectToAction("Login");
             }
         }
+
+        #region 获取密码 V2.0
+        /// <summary>
+        /// 获取密码 V2.0
+        /// </summary>
+        /// <param name="loginName"></param>
+        /// <returns></returns>
+        public string GetUserPassword(string loginName)
+        {
+            using (LoginDbContext db = new LoginDbContext())
+            {
+                var user = db.LoginModel.Where(o => o.UserName.ToLower().Equals(loginName));
+                if (user.Any())
+                    return user.FirstOrDefault().Password;
+                else
+                    return string.Empty;
+            }
+        } 
+        #endregion
+
+//        public bool IsUserInRole(string loginName, string roleName) {  
+//            using (LoginDbContext db = new LoginDbContext()) {  
+//                LoginModel SU = db.LoginModel.Where(o => o.UserName.ToLower().Equals(loginName)).FirstOrDefault();  
+//                if (SU != null) {
+//                    var roles = from q in db.LoginModel  
+//                                join r in db.LOOKUPRoles on q.LOOKUPRoleID equals r.LOOKUPRoleID  
+//                                where r.RoleName.Equals(roleName) && q.SYSUserID.Equals(SU.SYSUserID)  
+//                                select r.RoleName;  
+  
+//                    if (roles != null) {  
+//                        return roles.Any();  
+//                    }  
+//                }  
+  
+//                return false;  
+//            }  
+//}  
+
     }
 }
