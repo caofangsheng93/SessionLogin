e15efbf6a38bcf69cfbd98a5db57658453a1e809
caofangsheng93 <fangsheng93@163.com>
2016-04-12 16:21:37 +0800

    Session登陆案例

---
+++ LoginMVC/DAL/LoginDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LoginMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LoginMVC.DAL
+{
+    public class LoginDbContext:DbContext
+    {
+        public LoginDbContext()
+            : base("name=DbConnectionString")
+        { 
+        
+        }
+
+        public DbSet<LoginModel> LoginModel { get; set; }
+    }
+}
